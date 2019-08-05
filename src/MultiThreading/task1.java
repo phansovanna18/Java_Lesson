1970ea5669f9d0c653515c93c1a150330138cb8a
Vanna <phansovanna18@kit.edu.kh>
2019-08-05 20:50:35 +0700

    update

--- src/MultiThreading/task1.java
+++ src/MultiThreading/task1.java
@@ -1,7 +1,41 @@
 package MultiThreading;
 
+import java.security.PrivilegedExceptionAction;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 class task1 {
     public static void main(String[] args) {
-
+        Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                try{
+                    while (true){
+                        System.out.println(i++);
+                        Thread.sleep(1000);
+                    }
+                }catch (Exception a){}
+            }
+        });
+        a.start();
+        System.out.println("Hello world");
     }
 }
+
+class run extends Thread{
+    @Override
+    public void run() {
+
+        try{
+
+        while (true){
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            System.out.println( sdf.format(cal.getTime()) );
+            Thread.sleep(100);
+        }}catch (Exception a){}
+    }
+}
+
